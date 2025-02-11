58c56788d2111c3f5d6ce09323960c5c580b7b42
fergus-g <fergus.gildea@utrack.com>
2025-02-11 19:53:22 +0000

    Added cards to the deck list section

--- front-end/src/components/DeckList/DeckList.jsx
+++ front-end/src/components/DeckList/DeckList.jsx
@@ -1,11 +1,34 @@
+import { Button } from "@mui/material";
 import styles from "./DeckList.module.css";
+import { useState } from "react";
 
 export default function DeckList({ cards }) {
-  let cardsData = cards.data;
-  console.log(cardsData[0].name);
+  let cardsData = cards?.data;
 
-  const cardList = cardsData.map((item, index) => {
-    return <li key={index}>{item.name}</li>;
+  const cardList = cardsData?.map((item, index) => {
+    return (
+      <Button
+        sx={{
+          alignSelf: "center",
+          position: "relative",
+          width: "250px",
+          textAlign: "center",
+          marginTop: "10px",
+          color: "white",
+          textTransform: "none",
+          backgroundColor: "grey.800",
+          "&:hover": {
+            backgroundColor: "grey.700",
+          },
+        }}
+        key={index}
+        img={item.image_uri}
+        lang={item.lang}
+        id={item.type_line}
+      >
+        {item.name}
+      </Button>
+    );
   });
 
   return <div className={styles.container}>{cardList}</div>;
--- front-end/src/components/PlayArea/PlayArea.jsx
+++ front-end/src/components/PlayArea/PlayArea.jsx
@@ -1,5 +1,28 @@
 import styles from "./PlayArea.module.css";
 
 export default function PlayArea() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardzone}>
+        <div className={styles.creatureZone}>
+          <h1 className={styles.zoneHeading}>Creatures</h1>
+          <div className={styles.cardImages}></div>
+        </div>
+        <div className={styles.enchantmentZone}>
+          <h1 className={styles.zoneHeading}>Enchantments</h1>
+          <div className={styles.cardImages}></div>
+        </div>
+      </div>
+      <div className={styles.landsArtifacts}>
+        <div className={styles.landsZone}>
+          <h1 className={styles.zoneHeading}>Lands</h1>
+          <div className={styles.cardImages}></div>
+        </div>
+        <div className={styles.artifactsZone}>
+          <h1 className={styles.zoneHeading}>Artifacts</h1>
+          <div className={styles.cardImages}></div>
+        </div>
+      </div>
+    </div>
+  );
 }
