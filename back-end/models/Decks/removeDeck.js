804dde9b2090ff67aefcaa3adc26515220752d13
fergus-g <fergus.gildea@utrack.com>
2025-02-11 08:45:53 +0000

    Added delete functionality

---
+++ back-end/models/Decks/removeDeck.js
@@ -0,0 +1,41 @@
+import { pool } from "../../db/index.js";
+
+// export default async function removeDeck(id) {
+//   try {
+//     const poolInstance = await pool; // Ensure pool is correctly used
+//     const query = "DELETE FROM decks WHERE id = @id"; // Corrected SQL syntax
+
+//     const result = await poolInstance.request().input("id", id).query(query);
+
+//     return result.rowsAffected[0]; // Returns the number of deleted rows
+//   } catch (error) {
+//     console.error("Error deleting deck:", error);
+//     throw error;
+//   }
+// }
+
+export default async function removeDeck(id) {
+  const poolInstance = await pool;
+
+  try {
+    const transaction = poolInstance.transaction();
+    await transaction.begin();
+
+    await transaction
+      .request()
+      .input("id", id)
+      .query("DELETE FROM cards WHERE deck_id = @id");
+
+    await transaction
+      .request()
+      .input("id", id)
+      .query("DELETE FROM decks WHERE id = @id");
+
+    await transaction.commit();
+    return true;
+  } catch (error) {
+    console.error("Error deleting deck:", error);
+    await transaction.rollback();
+    throw error;
+  }
+}
--- back-end/routes/decksRoutes.js
+++ back-end/routes/decksRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import createDeck from "../controllers/Decks/createDeck.js";
 import getDecks from "../controllers/Decks/getDecks.js";
+import deleteDeck from "../controllers/Decks/deleteDeck.js";
 
 const router = express.Router();
 
 router.get("/:user_id", getDecks);
 router.post("/", createDeck);
+router.delete("/:id", deleteDeck);
 
 export default router;
