ead50941717d193a18beb2fb557ef85bf0253557
fergus-g <fergus.gildea@utrack.com>
2025-02-25 09:08:45 +0000

    Reverted eslint config changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,23 +2,10 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: { globals: globals.browser },
-    settings: {
-      react: {
-        version: "detect", // Auto-detect React version
-      },
-    },
-  },
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  {
-    files: ["**/*.test.js", "**/*.spec.js"],
-    rules: {
-      "react/prop-types": "off",
-    },
-  },
 ];
