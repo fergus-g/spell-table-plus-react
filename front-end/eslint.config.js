dd7c543caa4c5e29fc68d8d7d7faa2b72137f07b
fergus-g <fergus.gildea@utrack.com>
2025-02-25 08:59:33 +0000

    Removed linting from test files

--- front-end/eslint.config.js
+++ front-end/eslint.config.js
@@ -2,11 +2,21 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      // Add any general rules here if needed
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.test.js", "**/*.spec.js"],
+    rules: {
+      "react/prop-types": "off",
+    },
+  },
 ];
